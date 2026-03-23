a2046c8eedcbf7ef812626fde56629659a8a505a
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-23 18:12:05 -0500

    conexion errores

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { sequelize } from "./database/database.js";
 const port = 3000;
 
 function init(){
+    sequelize.authenticate()
+    .then(() => {
+        console.log("La conexión ha sido exitosa!!");
+    })
+    .catch((err) => {
+        console.error("Paila (BD)", err);
+    });
+
     app.listen(port, ()=>{
         console.log(`Aplicación escuchando en el puerto ${3000}`)
     })
