01950e184bbd021c5b57be10c40c556dedbda0be
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-23 18:21:05 -0500

    conexion errores tr c

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import { sequelize } from "./database/database.js";
 
 const port = 3000;
 
-async function init(){
-    await sequelize.authenticate()
-    .then(() => {
-        console.log("La conexión ha sido exitosa!!");
-    })
-    .catch((err) => {
-        console.error("Paila (BD)", err);
-    });
 
-    app.listen(port, ()=>{
-        console.log(`Aplicación escuchando en el puerto ${3000}`)
-    })
+async function init(){
+    try{
+        await sequelize.authenticate()
+        .then(() => {
+            console.log("La conexión ha sido exitosa!!");
+        })
+        .catch((err) => {
+            console.error("Paila (BD)", err);
+        });
+
+        app.listen(port, ()=>{
+            console.log(`Aplicación escuchando en el puerto ${3000}`)
+        })
+    }catch(error){
+        console.err("Paila", err);
+    }
+    
 }
 
 init();
