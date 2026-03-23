3f62b2c86e499890b2783e8598f655e8da9fed98
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-23 18:17:18 -0500

    conexion errores 2 fix xd

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 
-export const sequelize = new Sequelize("buyit_bd", "postgress", "sa", {
+export const sequelize = new Sequelize("buyit_db", "buy_it", "sa", { 
     port: 5432,
     host: "localhost",
-    dialect: "postgress",
+    dialect: "postgres",
 });
