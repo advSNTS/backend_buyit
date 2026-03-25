9d35cda7ba02f3a69540886c3e032b2a928b42b8
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-24 22:26:27 -0500

    changes

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -24,4 +24,6 @@ export const updateUser = async (req, res) => {
 export const deleteUser = async (req, res) => {
     const id = req.params.id;
     const user = await User.findByPk(id);
+    await user.destroy();
+    return res.sendStatus(204);
 }
