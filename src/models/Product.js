513218649336c60cdd14d167cb6c2a9722d7b2d6
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-24 21:42:26 -0500

    The only constant in life is change. — Heraclitus

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,2 +1,17 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database/database.js";
+
+export const Product = sequelize.define(
+    "products",
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    }
+)
