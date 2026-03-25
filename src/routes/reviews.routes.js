9e450b340d44e2205db491fbbc397fe8529f2a70
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-24 22:43:42 -0500

    changes

--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import { getReviews, createReview, updateReview, deleteReview } from "../controller/review.controller";
+
 const router = Router();
 
-router.get("/reviews", {})
-router.post("/reviews", {})
-router.put("/reviews/:id", {})
+router.get("/reviews", getReviews)
+router.post("/reviews", createReview)
+router.put("/reviews/:id", updateReview)
 router.delete("/reviews/:id", {})
 
 export default router
