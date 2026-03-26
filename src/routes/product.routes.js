37a67b6a4393f63afd3ff5986340fb996b9c14f1
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-25 21:46:16 -0500

    cambios

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,3 +1,7 @@
 import { Router } from "express";
 
 import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from "../controller/productController";
+
+const router = Router();
+
+router.get("/products", getProducts)
