3514f6b430e09a89adf8071aee8afc6e7a4a53c8
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-25 21:47:25 -0500

    cambios

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import userRoutes from "./routes/users.routes.js"
 import reviewRoutes from "./routes/reviews.routes.js"
+import productRoutes from "./routes/product.routes.js"
 
 const app = express();
 app.use(express.json());
 app.use(userRoutes);
 app.use(reviewRoutes);
+app.use(productRoutes);
 export default app
