b0aa5c9186bece1bdd1e9bde8f2c91153280f194
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-25 21:54:59 -0500

    cambios

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -7,6 +7,6 @@ const router = Router();
 router.get("/products", getProducts);
 router.post("/products", createProduct);
 router.put("/products/:id", updateProduct);
-
+router.delete("/products/:id", deleteProduct);
 
 export default router;
